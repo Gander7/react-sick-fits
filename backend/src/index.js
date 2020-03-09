3bc296f7cad6fa2d686aafa6b1a493416489ce10
Zach Allen <zachjallen@gmail.com>
2020-03-09 15:50:33 -0300

    setup prisma yoga

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,18 @@
+require('dotenv').config({ path: 'variables.env' })
+const createServer = require('./createServer')
+const db = require('./db')
+
 // let's go!
+const server = createServer()
+
+// TODO use express middleware to handle cookies (JWT)
+// TODO use express middleware to populate users
+
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  },
+}, details => {
+  console.log(`Server is now running on ${details.port}`)
+})
