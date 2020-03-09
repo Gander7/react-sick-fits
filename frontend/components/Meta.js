beec360265f983d4646dd214a49f598a5cc9ae2c
Zach Allen <zachjallen@gmail.com>
2020-03-09 13:44:10 -0300

    fixed flicker on refresh

--- frontend/components/Meta.js
+++ frontend/components/Meta.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 const Meta = () => (
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <meta charset="utf=8" />
+    <meta charSet="utf=8" />
     <link rel="shortcut icon" href="/static/favicon.ico" type="image/x-icon"/>
     <link rel="stylesheet" type="text/css" href="/static/nprogress.css"/>
     <title>Sick Fits!</title>
