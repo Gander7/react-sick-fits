0833b5983f15b5073079c12a857c887e79722b11
Zach Allen <zach@basebridge.com>
2020-04-14 11:29:21 -0300

    added user relationship to item and protected sell page when not logged in

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -8,10 +8,19 @@ const { transport, makeANiceEmail } = require('../mail')
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: Check to see if they are logged in
+    if (!ctx.request.userId) throw new Error('You must be logged in to do that!')
 
     const item = await ctx.db.mutation.createItem(
       {
-        data: { ...args },
+        data: {
+          // relationship between item and user
+          user: {
+            connect: {
+              id: ctx.request.userId,
+            },
+          },
+          ...args,
+        },
       },
       info,
     )
