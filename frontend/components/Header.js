6abcbd3dcfa17f05c33e72243fc8c5127eeae480
Zach <zachjallen@gmail.com>
2020-05-11 11:53:08 -0300

    added basic search

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -4,6 +4,7 @@ import NProgress from 'nprogress'
 import styled from 'styled-components'
 import Nav from './Nav'
 import Cart from './Cart'
+import Search from './Search'
 
 Router.onRouteChangeStart = () => {
   NProgress.start()
@@ -65,7 +66,7 @@ const Header = (props) => {
         <Nav />
       </div>
       <div className="sub-bar">
-        <p>Search</p>
+        <Search />
       </div>
       <Cart />
     </StyledHeader>
---
+++ frontend/components/Search.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import Downshift from 'downshift'
+import Router from 'next/router'
+import { ApolloConsumer } from 'react-apollo'
+import gql from 'graphql-tag'
+import debounce from 'lodash.debounce'
+
+import { DropDown, DropDownItem, SearchStyles } from './styles/DropDown'
+
+const SEARCH_ITEMS_QUERY = gql`
+  query SEARCH_ITEMS_QUERY($term: String!) {
+    items(where: { OR: [{ title_contains: $term }, { description_contains: $term }] }) {
+      id
+      image
+      title
+    }
+  }
+`
+
+class Autocomplete extends React.Component {
+  state = {
+    items: [],
+    loading: false,
+  }
+
+  onChange = debounce(async (e, client) => {
+    this.setState({ loading: true })
+    // Manually Query Apollo Client
+    const res = await client.query({
+      query: SEARCH_ITEMS_QUERY,
+      variables: { term: e.target.value },
+    })
+    this.setState({ items: res.data.items, loading: false })
+  }, 400)
+
+  render() {
+    return (
+      <SearchStyles>
+        <div>
+          <ApolloConsumer>
+            {(client) => (
+              <input
+                type="search"
+                onChange={(e) => {
+                  e.persist()
+                  this.onChange(e, client)
+                }}
+              />
+            )}
+          </ApolloConsumer>
+
+          <DropDown>
+            {this.state.items.map((item) => {
+              return (
+                <DropDownItem key={item.id}>
+                  <img width="50" src={item.image} alt={item.title} />
+                  {item.title}
+                </DropDownItem>
+              )
+            })}
+          </DropDown>
+        </div>
+      </SearchStyles>
+    )
+  }
+}
+
+export default Autocomplete
