b618fda3a66a5ef439bf0f9e2be0d74082ed7608
Zach <zachjallen@gmail.com>
2020-05-11 10:56:29 -0300

    added cart count to navbar

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -4,6 +4,7 @@ import User from './User'
 import Signout from './Signout'
 import { TOGGLE_CART_MUTATION } from '../components/Cart'
 import { Mutation } from 'react-apollo'
+import CartCount from './CartCount'
 
 const Nav = () => {
   const links = [
@@ -34,7 +35,14 @@ const Nav = () => {
               </Link>
               <Signout />
               <Mutation mutation={TOGGLE_CART_MUTATION}>
-                {(toggleCart) => <a onClick={toggleCart}>My Cart</a>}
+                {(toggleCart) => (
+                  <a onClick={toggleCart}>
+                    My Cart
+                    <CartCount
+                      count={me.cart.reduce((tally, item) => tally + item.quantity, 0)}
+                    ></CartCount>
+                  </a>
+                )}
               </Mutation>
             </>
           )}
